2fba9e8a6e22fd2fd10923a447119822ac4e9d29
Yi Miao <yi.matheson@gmail.com>
2016-09-20 21:50:45 -0400

    added node-modules for express js, added routes and server.js

---
+++ routes/rest.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.post("/urls", function (req, res) {
+    var shortUrl = "";
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var restRouter = require('./routes/rest');
+var redirectRouter = require('./routes/redirect');
+
+app.use("/api/v1", restRouter);
+
+app.use(":shortUrl", redirectRouter);
+//app.get("/", function (req, res) {
+//    res.send("hello express world");
+//});
+
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+//var http = require("http");
+//var fs = require("fs");
+//
+//http.createServer(function (req, res) {
+//    if (req.url === "/") {
+//        res.writeHead(200, {"Content-Type": "text-html"});
+//        var html = fs.readFileSync(__dirname + "/index.html");
+//        res.end(html);
+//    }
+//
+//    if (req.url === "/api") {
+//        res.writeHead(200, {"Content-Type": "application/json"});
+//        var obj = {
+//            name: "Yi Miao",
+//            age: 12
+//        }
+//        res.end(JSON.stringify(obj));
+//    }
+//}).listen(3000);
